1724307320f19b25e00cc0a6456e9b507f94e7e4
JuniiorMendes <juniiormendesofc@gmail.com>
2024-08-22 19:51:15 -0300

    adicionar imagens e implementação em main.py

--- main.py
+++ main.py
@@ -5,17 +5,37 @@ from pygetwindow import *
 
 class Microsip:
     def __init__(self):
-        self._program: Win32Window = getWindowsWithTitle("MicroSIP")[0]
+        self.program: Win32Window = getWindowsWithTitle("MicroSIP")[0]
     
     def get_size(self):
-        return (self._program.size.width, self._program.size.height)
+        return (self.program.size.width, self.program.size.height)
 
     def get_position(self):
-        return (self._program.top, self._program.left)
+        return (self.program.top, self.program.left)
 
+    def mouseMove(self):
+        moveTo(20,10, duration=3)
 
-def main() -> None:
-    return None
+    def localization(self, imgDir):
+        return locateOnScreen(imgDir)
+
+def main():
+    micro = Microsip()
+    micro.program.activate()
+    local1 = micro.localization("img/captura01.PNG")
+    moveTo(local1[0], local1[1], duration = 1)
+    moveTo(local1[0], local1[1] - 25)
+    click()
+    typewrite("2688", interval = 0.5)
+
+    try:
+        local2 = micro.localization("img/captura02.PNG")
+        moveTo(local2[0], local2[1], duration = 1)
+        click()
+    except Exception() as err:
+        pass
+
 
 if __name__ == "__main__":
     main()
+

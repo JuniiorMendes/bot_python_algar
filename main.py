5afb83e3aaf62c82d177f3d46230f5c00ee8d2a2
YumemiReis <adrianoreispereira88@gmail.com>
2024-08-21 21:01:23 -0300

    chore/creating-microsip-class

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 
-def main() -> None:
-    print("Hello world!")
+from pyautogui import *
+from pygetwindow import *
+
+class Microsip:
+    def __init__(self):
+        self._program: Win32Window = getWindowsWithTitle("MicroSIP")[0]
     
+    def get_size(self):
+        return (self._program.size.width, self._program.size.height)
+
+    def get_position(self):
+        return (self._program.top, self._program.left)
+
+
+def main() -> None:
     return None
 
 if __name__ == "__main__":

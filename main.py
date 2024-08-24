444e1d1874f2f14df0d7bc448485008d7a0dd769
JOAO RICARDO ESTEVAM DA SILVA <joaoreds@algartech.com>
2024-08-24 19:56:54 -0300

    Inicialização de configurações

--- main.py
+++ main.py
@@ -1,70 +1,17 @@
-from tkinter import *
-from tkinter import ttk
-from MicroSIP_lib.MicroSIP import *
-from time import sleep
+from pyautogui import *
+from pygetwindow import *
 
-import re
+programsName = getAllTitles()
 
-number_pattern = r"^\d{1,12}$"
+program = None
 
-def main():
-    root = Tk()
+for name in programsName :
+    if "MicroSIP" in name :
+        program = getWindowsWithTitle(name)[0]
 
-    ramal = StringVar()
+# left, top, _,_ = locateOnScreen("img/seta.png")
+# moveTo(left, top, duration = 2)
+# click()
 
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Entre com o ramal ou numero de telefone: ").grid(column=0, row=0)
-    dadosEntry = ttk.Entry(frm, textvariable=ramal)
-    dadosEntry.grid(column=1, row=0)
-    ttk.Button(frm, text="Start", command = root.destroy).grid(column=2, row=0)
-    root.mainloop()
-
-    ramal.set(str(re.findall(number_pattern, str(ramal.get()))[0]))
-
-    if(len(str(ramal.get())) == 0):
-        print("exitting...")
-        exit()
-
-    press("super")
-    sleep(2)
-    typewrite("microsip", interval=0.5)
-    press("enter")
-    sleep(2)
-
-    for name in getAllTitles():
-        if "MicroSIP" in name:
-            program_name = name
-
-    microsip = Microsip(program_name)
-    microsip.focus()
-
-    width, height = microsip.get_size()
-    left, top = microsip.get_position()
-
-    centerx, centery = \
-        microsip.program.center.x, \
-        microsip.program.center.y
-
-    percent_width = \
-        width / microsip.fixed_size[0]
-
-    percent_height = \
-        height / microsip.fixed_size[1]
-
-    moveTo(centerx, centery - (5 * 30) * percent_height, duration=1)
-    click()
-
-    hotkey("ctrl", "a")
-    press("delete")
-
-    if(len(str(ramal.get())) == 11):
-        ramal.set("09" + str(ramal.get()))
-
-    typewrite(str(ramal.get()), interval=0.5)
-
-    press("enter")
-
-if __name__ == "__main__":
-    main()
-
+program.activate()
+hotkey("ctrl", "p")
